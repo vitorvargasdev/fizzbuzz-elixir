656feefe1d25cd4eac2814ad6178536f6d70dfc4
vitorvargasdev <instalando@protonmail.com>
2023-09-18 22:45:34 -0300

    feat: fizzbuzz project

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzbuzzTest do
   use ExUnit.Case
-  doctest Fizzbuzz
 
-  test "greets the world" do
-    assert Fizzbuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      assert Fizzbuzz.build("numbers.txt") ==
+               {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+    end
+
+    test "when an invalid file is provided, returns an error" do
+      assert Fizzbuzz.build("invalid_file.txt") ==
+               {:error, "Error reading the file: enoent"}
+    end
   end
 end
